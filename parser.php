a922fe16f92e86141d2ba421a9fddd3fc5cf4eb1
alha4 <a.i.n.1989@mail.ru>
2015-01-14 23:45:37 +0500

    альфа версия

---
+++ parser.php
@@ -0,0 +1,51 @@
+<?php
+ ini_set("display_errros","on");
+ ini_set('error_reporting', E_ALL);
+ error_reporting(E_ALL);
+ 
+ namespace Alpha\SimpleParser;
+ 
+ class WebPageParser {
+    
+    private $webPagesURI;
+    
+    function __construct($url) {
+        $this->webPagesURI = $url;
+        //$this->load();
+    }
+    public function load() {
+      try {
+        if(!$this->is_loaded_curl_module()) {
+         
+           throw new Exception("Не установлен модуль расширение [curl]");
+        }
+        $pages_url = $this->webPagesURI;
+        
+        $mс = curl_multi_init();
+        
+        foreach($pages_url as $url) {
+            
+        }
+        
+      } catch(Exception $err) {
+         echo 'errir load modules';  
+         echo $err->getMessage(); 
+      }
+    }
+    private function is_loaded_curl_module() {
+
+        if( !extension_loaded("curl") ) {
+           return false;
+        }
+        return true;
+    }
+    public function parse() {
+        echo 'тест';
+    }
+ }
+ 
+ $webPageParser = new WebPageParser(array("https://www.fabrikant.ru/market/?action=list_public_auctions&type=531&status_group=sg_published"));
+ $webPageParser->load();
+ 
+ 
+?>
